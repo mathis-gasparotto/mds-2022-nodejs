1de5c52d791c78a47e76d6d831af7c29108b4e8f
Mathis-Gasparotto <78808807+Mathis-Gasparotto@users.noreply.github.com>
2023-01-04 12:20:10 +0100

    :memo: update chat project folder name

--- ws/src/routes/getLogout.ts
+++
@@ -1,9 +0,0 @@
-import { Application } from "express-ws"
-
-export function getLogout (app: Application) {
-  app.get('/logout', (req, res) => {
-    res.clearCookie('ssid')
-    res.redirect('/login')
-    return
-  })
-}
--- ws/src/routes/getRegister.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from "express-ws"
-import path from 'path'
-
-export function getRegister(app: Application) {
-  app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../pages/register/index.html'))
-  })
-}
